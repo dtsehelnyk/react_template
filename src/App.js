65ec2eb77ce70e10510acbea53eb30dfdd48b68c
Dmytro Tsehelnyk <34zimnii@gmail.com>
2021-02-05 22:45:42 +0200

    template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import './styles/reset.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{fontSize: 140 + "px"}}>it works</h1>
+    </div>
+  );
+}
+
+export default App;
